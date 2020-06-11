dd147f01be341d85e8dcc5b2c79384343083ad58
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-06-11 09:32:28 +0200

    Update kueche-off.sh

--- kueche-off.sh
+++ kueche-off.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pwd
 source /home/pi/pi-music-bot/IPs.py
-ssh $kueche_IP 'sudo shutdown -h now'
-sleep 20
+# ssh $kueche_IP 'sudo shutdown -h now' #update 2020: kueche immer an!
+# sleep 20
 /home/pi/hs100/hs100.sh off -i $plug_kueche_IP #kueche
 
 
