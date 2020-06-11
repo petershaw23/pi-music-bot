30949a0abec514fbac32cd3bc131946669a7c2aa
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-06-11 09:27:31 +0200

    Update alles-off.sh

--- alles-off.sh
+++ alles-off.sh
@@ -4,11 +4,11 @@ pwd
 source /home/pi/pi-music-bot/IPs.py
 
 ssh $schlafzi_IP 'sudo shutdown -h now' #schlafzi
-#ssh $wohnzi_IP 'sudo shutdown -h now' #wohnzi hat nun raspbian drauf!
-ssh $kueche_IP 'sudo shutdown -h now' #kueche hat nun raspbian drauf!
+ssh $wohnzi_IP 'sudo shutdown -h now' #wohnzi hat nun raspbian drauf!
+#ssh $kueche_IP 'sudo shutdown -h now' #kueche hat nun raspbian drauf! update 2020: kueche immer an!
 sleep 18
 /home/pi/hs100/hs100.sh off -i $plug_schlafzi_IP #schlafzi dose
 sleep 4
 /home/pi/hs100/hs100.sh off -i $plug_kueche_IP #kueche
-#sleep 1
-#/home/pi/hs100/hs100.sh off -i $plug_wohnzi_IP #wohnzimmer dose
+sleep 1
+/home/pi/hs100/hs100.sh off -i $plug_wohnzi_IP #wohnzimmer dose
